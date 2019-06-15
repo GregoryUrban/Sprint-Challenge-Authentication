5096cdea11c5286aef7c77caf29d1f1584db641a
GregoryUrban <turknben@gmail.com>
2019-06-14 21:48:30 -0400

    MVP - Finished register and login functions, added routes-model, tokenGenerator, .env

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt = require("bcryptjs");
+const tokenService = require("../auth/token-service.js");
+
+const Users = require("./routes-model.js");
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -10,10 +14,39 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 5);
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      const token = tokenService.generateToken(user);
+      res.status(201).json({ saved, message: `registered, ${token}` });
+    })
+    .catch(error => {
+      res.status(500).json({error, message: 'Error registering user'});
+    });
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenService.generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!, have a token...`,
+          token,
+          roles: token.roles // for user roles
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({error, message: '500 failure on logging in'}); // was just (error)
+    });
 }
 
 function getJokes(req, res) {
